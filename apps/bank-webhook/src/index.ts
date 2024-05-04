64fe6b0b44ae07bb83ec68f45028788a31fa6541
siliconBorn <nevilleblast2238@gmail.com>
2024-05-04 10:55:42 +0530

    minor changes

--- apps/bank-webhook/src/index.ts
+++ apps/bank-webhook/src/index.ts
@@ -5,7 +5,6 @@ const app = express();
 app.use(express.json())
 
 app.post("/hdfcWebhook", async (req, res) => {
-    //TODO: Add zod validation here?
     const paymentInformation: {
         token: string;
         userId: string;
